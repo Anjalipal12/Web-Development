c070a60288294bea29236847306297c851685c75
Anjali <anjalipal2912@gmail.com>
2025-09-14 02:29:53 +0530

    add updated code

---
+++ 13-JS-(part-1)/app.js
@@ -0,0 +1,14 @@
+let pencilPrice = 10;
+let eraserPrice = 5;
+let output = `The total price : ${pencilPrice + eraserPrice}Rupees`;
+console.log(output);
+
+// arithmetic operator
+let a = 10;
+let b = 5;
+console.log(a+b); 
+console.log(a-b); 
+console.log(a*b); 
+console.log(a**b); 
+console.log(a/b); 
+console.log(a%b); 
